261872e731c6009a19f12eb2a86886bc9f271d30
Shashank Rai <shashank.xx92@gmail.com>
2023-08-26 04:45:20 +0530

    commit

--- provider.tf
+++ provider.tf
@@ -7,30 +7,9 @@ terraform {
   }
 }
 
-data "google_service_account_access_token" "default" {
- provider               	= google.impersonation
- target_service_account 	= local.terraform_service_account
- scopes                 	= ["userinfo-email", "cloud-platform"]
- lifetime               	= "1200s"
-}
-
 provider "google" {
- alias = "impersonation"
- scopes = [
-   "https://www.googleapis.com/auth/cloud-platform",
-   "https://www.googleapis.com/auth/userinfo.email",
- ]
+  # Configuration options
+  project = "terraform-gcp-382215"
+  region = "us-central1"
+  zone = "us-central1-a"
 }
-
-provider "google" {
- project = "terraform-gcp-382215"
- region = "us-central1"
- zone = "us-central1-a"
- access_token	= data.google_service_account_access_token.default.access_token
- request_timeout 	= "60s"
-}
-
-locals {
- terraform_service_account = "terraform-gcp@terraform-gcp-382215.iam.gserviceaccount.com"
-}
-
